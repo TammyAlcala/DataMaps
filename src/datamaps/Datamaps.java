2cece6dfa66538a8e849501711cd8e716eb37217
Tammy <Gian@Gian-PC>
2013-03-19 09:06:16 -0400

    Inicio do projeto

---
+++ src/datamaps/Datamaps.java
@@ -0,0 +1,37 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datamaps;
+
+/**
+ *
+ * @author Tammy
+ */
+
+import wicket.contrib.gmap.api.GClientGeocoder;
+import java.util.ArrayList;
+import datamaps.place;
+
+public class Datamaps {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        /*
+         recebe lista localidade , palavra chave
+         * função encode dados
+         * retorna lat/lng
+         * constrói query
+         * object json
+         * retorna json com dados
+         
+         
+         */
+        
+        // TODO code application logic here
+    }
+}
+
---
+++ src/datamaps/place.java
@@ -0,0 +1,47 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datamaps;
+
+/**
+ *
+ * @author Tammy
+ */
+
+ 
+
+public class place {
+    
+    String cidade;
+    String UF;
+    String pais;
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getUF() {
+        return UF;
+    }
+
+    public void setUF(String UF) {
+        this.UF = UF;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+    
+ 
+     
+    
+}
